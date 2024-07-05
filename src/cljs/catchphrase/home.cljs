ca3ed04e2dd8236d9f4497e33c37f674e897f082
g-jensen <greg.micah.jensen@gmail.com>
2024-07-05 03:03:18 -0400

    homepage design

--- src/cljs/catchphrase/home.cljs
+++ src/cljs/catchphrase/home.cljs
@@ -14,7 +14,21 @@
     (ws/call! :room/create {:nickname nickname} navigate-to-room!)))
 
 (defn home [nickname-ratom]
-  [:div.homepage-container
+  [:div [:div {:style {:width "50%"
+                      :height "100%"
+                      :background-color "#a7584b"
+                      :position "absolute"
+                      :z-index "-1"
+                      :left "50%"
+                      :overflow "hidden"}}]
+   [:div {:style {:width "50%"
+                  :height "100%"
+                  :background-color "#537d8b"
+                  :position "absolute"
+                  :z-index "-1"
+                  :overflow "hidden"}}]
+   [:div.homepage-container
+        {:style {:color "#f4f3de"}}
    [:h1 "Welcome to catchphrase"]
    [:div.nickname-input
     [:input
@@ -27,7 +41,7 @@
     [:button
      {:id       "-create-room-button"
       :on-click #(create-room! @nickname-ratom)}
-     "Create Room"]]])
+     "Create Room"]]]])
 
 (defmethod page/render :home [_]
   [home state/nickname])
