cf77f633469fbe4041906b7b8cb241571c8919f5
Alex Jensen <alex@endure.io>
2026-06-22 21:53:54 -0400

    Revert "Migrates to Jetty for observability"
    This reverts commit 63d0651815de7507e2e4e94a6ef03e5945834aa0.

--- src/clj/catchphrase/http.clj
+++ src/clj/catchphrase/http.clj
@@ -9,7 +9,7 @@
             [c3kit.wire.jwt :refer [wrap-jwt]]
             [compojure.core :refer [defroutes]]
             [compojure.route :as route]
-            [ring.adapter.jetty :refer [run-jetty]]
+            [org.httpkit.server :refer [run-server]]
             [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
             [ring.middleware.content-type :refer [wrap-content-type]]
             [ring.middleware.cookies :refer [wrap-cookies]]
@@ -72,13 +72,13 @@
 (defn start [app]
   (let [port (or (some-> "PORT" System/getenv Integer/parseInt) 8123)]
     (log/info (str "Starting HTTP server: http://localhost:" port))
-    (let [server (run-jetty root-handler {:port port :join? false})]
+    (let [server (run-server root-handler {:port port})]
       (assoc app :http server))))
 
 (defn stop [app]
-  (when-let [server (:http app)]
+  (when-let [stop-server-fn (:http app)]
     (log/info "Stopping HTTP server")
-    (.stop server))
+    (stop-server-fn :timeout 1000))
   (dissoc app :http))
 
 
